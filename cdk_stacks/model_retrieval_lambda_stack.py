35338d6af9fcfb3b37003e0478ad9c612a19961d
Matt Kenney <mtnkenney2@gmail.com>
2020-11-24 21:46:18 -0500

    working lambda with working Elastic File System (EFS) and API Gateway integration

--- cdk_stacks/model_retrieval_lambda_stack.py
+++ cdk_stacks/model_retrieval_lambda_stack.py
@@ -1,5 +1,3 @@
-import boto3
-from botocore.exceptions import ClientError
 from aws_cdk import (
     core as cdk,
     aws_lambda as _lambda,
@@ -9,16 +7,6 @@ from aws_cdk import (
     aws_efs as efs
 )
 
-import time
-import logging
-from efsync.utils.config.load_config import load_config
-from efsync.utils.ssh.ssh_key import create_ssh_key, delete_ssh_key
-from efsync.utils.ec2.ec2_main import terminate_ec2_instance
-from efsync.utils.ec2.ec2_waiter import wait_for_ec2
-from efsync.utils.ec2.create_user_data import create_user_data
-from efsync.utils.iam_profile.iam_profile import create_iam_profile, delete_iam_profile
-from efsync.utils.ec2.custom_waiter import custom_waiter
-
 
 class ModelRetrievalLambda(cdk.Stack):
 
@@ -26,7 +14,8 @@ class ModelRetrievalLambda(cdk.Stack):
         """ Deploy the DynamoDB Database and Data Tables """
         super().__init__(scope, id, **kwargs)
 
-        vpc = ec2.Vpc(self, 'ModelRetrievalLambdaVpc', max_azs=2)
+        # Create private VPC to store lambda + EFS inside of
+        vpc = ec2.Vpc(self, 'ModelRetrievalLambdaVpc', max_azs=2, nat_gateways=1)
 
         lambda_security_group = ec2.SecurityGroup(self, 'ModelRetrievalLambdaSg',
                                                   vpc=vpc, allow_all_outbound=False,
@@ -35,6 +24,7 @@ class ModelRetrievalLambda(cdk.Stack):
         efs_security_group = ec2.SecurityGroup(self, 'ModelRetrievalLambdaEfsSg',
                                                vpc=vpc, security_group_name='ModelRetrievalLambdaEfsSg')
 
+        # Allow lambda to mount the EFS
         lambda_security_group.connections.allow_to(efs_security_group, ec2.Port.tcp(2049))  # EFS ingress
         lambda_security_group.connections.allow_to_any_ipv4(ec2.Port.all_tcp())  # explicitly allow all outbound
 
@@ -44,8 +34,7 @@ class ModelRetrievalLambda(cdk.Stack):
             'ModelRetrievalLambdaFileSystem',
             vpc=vpc,
             security_group=efs_security_group,
-            throughput_mode=efs.ThroughputMode.PROVISIONED,
-            provisioned_throughput_per_second=cdk.Size.mebibytes(50),
+            throughput_mode=efs.ThroughputMode.BURSTING, # Bursting mode adjusts throughput based on usage
             removal_policy=cdk.RemovalPolicy.DESTROY
         )
 
@@ -62,7 +51,7 @@ class ModelRetrievalLambda(cdk.Stack):
         lambda_dir = './lambdas/model_retrieval_lambda'
         self.lambda_function = _lambda.Function(
             self,
-            'ModelRetrievalLambda',
+            'ModelRetrievalLambdaFunction',
             vpc=vpc,
             security_group=lambda_security_group,
             function_name='ModelRetrievalLambdaFunction',
@@ -70,102 +59,65 @@ class ModelRetrievalLambda(cdk.Stack):
             code=_lambda.Code.from_asset(lambda_dir),  # directory where code is located
             handler='get_models.lambda_handler',  # the function the lambda invokes,
             memory_size=1024,  # May need a large amount of memory to download and store models temporarily
-            timeout=cdk.Duration.seconds(5),
+            timeout=cdk.Duration.seconds(60),  # may take a long time to load syft for the first time
             filesystem=_lambda.FileSystem.from_efs_access_point(access_point, '/mnt/python')
         )
 
         # Allow the above fn to provide the lambda with required permissions for EFS/ VPC execution, then add the rest
         add_permissions_to_lambda(self.lambda_function.role)
 
-        # Create the REST API
+        # Create the REST API and allow it to invoke the lambda
+        # TODO: This lambda is open to the world right now. Figure out how to use passed IAM principles to gate access.
         self.api = apigateway.LambdaRestApi(
-             self,
-             'ModelRetrievalLambdaAPI',
-             handler=self.lambda_function,
-             policy=create_access_policy(iam_principals),
+            self,
+            'ModelRetrievalLambdaAPI',
+            handler=self.lambda_function,
         )
 
-        # Save the variables that we need for the post-deploy actions
-        cdk.CfnOutput(self, 'efsFilesystemId', value=fs.file_system_id)
-        cdk.CfnOutput(self, 'efsSecurityGroupId', value=lambda_security_group.security_group_id)
-        cdk.CfnOutput(self, 'efsSubnetId', value=vpc.private_subnets[0].subnet_id)
+        # Create an EC2 instance to populate EFS with pip dependencies
+        # This EC2 instance is deleted once done installing dependencies in EFS via the 'post_deploy_actions' script
+        requirements_string = read_requirements_from_file(lambda_dir + '/requirements.txt')
+        temp_userdata = ec2.UserData.for_linux(shebang="#!/bin/bash")
+        temp_userdata.add_commands(
+            'sudo yum update -y',
+            'sudo yum install -y amazon-efs-utils',
+            'sudo mkdir efs',
+            'sudo mount -t efs -o tls ' + str(fs.file_system_id) + ':/ efs',
+            'sudo chown -R ec2-user:ec2-user efs',
+            'sudo rm -rf efs/*',
+            'sudo yum install docker -y',
+            'sudo service docker start',
+            'sudo docker run -v "$PWD":/var/task lambci/lambda:build-python3.7 pip3 --no-cache-dir install -t efs' +
+            str(access_point_path) + ' ' + str(requirements_string),
+            'aws ec2 create-tags --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) ' +
+            '--tags \'Key=\"user_data\",Value=True\' --region ' + self.region
+        )
 
-        # This is super hacky - but for now just set this to true if you want to do the post deploy action and deploy
-        # pip dependencies
-        post_deploy = False
+        temp_instance = ec2.Instance(
+            self, 'ModelRetrievalLambdaEc2Instance',
+            instance_name='ModelRetrievalLambdaEc2Instance',
+            vpc=vpc,  # same VPC as EFS and lambda
+            security_group=lambda_security_group,  # Same security group as lambda so that it can access EFS
+            instance_type=ec2.InstanceType('t2.micro'),
+            machine_image=ec2.MachineImage.generic_linux({self.region: 'ami-04bf6dcdc9ab498ca'}),  # Amazon Linux 2
+            block_devices=[ec2.BlockDevice(device_name='/dev/sdh', volume=ec2.BlockDeviceVolume.ebs(20))],
+            user_data=temp_userdata
+        )
 
-        if post_deploy:
+        # Instance will tag itself as done (see last line of user data) once its finished installing dependencies
+        # Post deploy script will check to ensure that the Ec2 instance is done before deleting it
+        create_tags_permissions = iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                "ec2:DeleteTags",
+                "ec2:CreateTags"
+            ],
+            resources=['*']
+        )
+        temp_instance.role.add_to_policy(create_tags_permissions)
 
-            try:
-                outputs = boto3.Session().client("cloudformation").describe_stacks(
-                    StackName=self.stack_name)["Stacks"][0]["Outputs"]
-
-                output_dict = {}
-                for output in outputs:
-                    key = output['OutputKey']
-                    value = output['OutputValue']
-                    output_dict[key] = value
-
-            except ClientError:
-                print('Not deployed yet')
-                return
-
-            except KeyError:
-                print('Outputs not properly configured')
-                return
-
-            # Install pip dependencies using efsync
-            config = {
-                'efs_filesystem_id': output_dict['efsFilesystemId'],  # aws efs filesystem id (moint point)
-                'default_sec_id': output_dict['efsSecurityGroupId'],  # security group associated with the EFS
-                'security_group': output_dict['efsSecurityGroupId'],  # security group associated with the EFS
-                'subnet_Id': output_dict['efsSubnetId'],  # subnet of which the efs is running in
-                'ec2_key_name': 'efsync-asd913fjgq3',  # required key name for starting the ec2 instance
-                'clean_efs': 'all',  # Defines if the EFS should be cleaned up before. values: `'all'`,`'pip'`,
-                'aws_profile': 'default',  # aws iam profile with required permission configured in .aws/credentials
-                'aws_region': self.region,  # the aws region where the efs is running
-                'efs_pip_dir': access_point_path,  # pip directory on ec2
-                'python_version': 3.7,
-                # python version used for installing pip packages -> should be used as lambda runtime afterwards
-                'requirements': lambda_dir + '/requirements.txt',
-                # path + file to requirements.txt which holds the installable pip packages
-            }
-            logger = logging.getLogger('efsync')
-            logger.setLevel(logging.DEBUG)
-
-            start = time.time()
-            logger.info('starting....')
-
-            # load config
-            logger.info('loading config')
-            config = load_config(config)
-
-            # creating ssh key for scp in memory
-            logger.info('creating ssh key for scp in memory')
-            try:
-                config['key'] = create_ssh_key(config['bt3'],  config['ec2_key_name'])
-            except Exception as e:
-                raise e
-
-            # starts ec2 instance in vpc with security group and ssh key
-            logger.info(f"starting ec2 instance with security group "
-                        f"{config['security_group']} and subnet_Id {config['subnet_Id']}")
-            config['instance_id'] = create_ec2_instance(config)
-
-            # stopping and deleting every resource
-            # ec2
-            logger.info(f"stopping ec2 instance with instance id {config['instance_id']}")
-            terminate_ec2_instance(bt3=config['bt3'], instance_id=config['instance_id'])
-            # iam profile
-            logger.info(f"deleting iam profile")
-            delete_iam_profile(config)
-            # ssh key
-            logger.info(f"deleting ssh key")
-            delete_ssh_key(config['bt3'], config['key']['name'])
-
-            logger.info(
-                f'#################### finished after {round(time.time() - start, 2) / 60} '
-                f'minutes ####################')
+        # Save the variables that we need for the post-deploy actions script
+        cdk.CfnOutput(self, 'ec2InstanceId', value=temp_instance.instance_id)
 
 
 def add_permissions_to_lambda(lambda_role):
@@ -186,45 +138,17 @@ def create_access_policy(iam_principals):
     return policy_doc
 
 
-def create_ec2_instance(config: dict = None):
+def read_requirements_from_file(file_path):
     try:
-        ec2_client = config['bt3'].resource('ec2')
-        instance_profile = create_iam_profile(config)
-        user_data = create_user_data(config)
-        # was to fast for aws after creation
-        time.sleep(10)
-        instance = ec2_client.create_instances(
-            BlockDeviceMappings=[
-                {
-                    'DeviceName': '/dev/sdh',
-                    'VirtualName': 'ephemeral0',
-                    'Ebs': {
-                        'DeleteOnTermination': True,
-                        'VolumeSize': 10,
-                        'VolumeType': 'gp2',
-                        'Encrypted': False,
-                    },
-                },
-            ],
-            ImageId='ami-04bf6dcdc9ab498ca',
-            MinCount=1,
-            MaxCount=1,
-            InstanceType='t2.micro',
-            UserData=user_data,
-            SecurityGroupIds=[
-                config['security_group'],
-                config['default_sec_id'],
+        # read all requirements from file and remove \n
+        install_list = open(file_path, 'r').read().splitlines()
+        # removes empty lines and comments
+        install_requires = [
+            str(requirement)
+            for requirement
+            in install_list if not requirement.startswith('#') and len(requirement) > 0]
+        return ' '.join(install_requires)
 
-            ],
-            SubnetId=config['subnet_Id'],
-            KeyName=config['key']['name'],
-            IamInstanceProfile={'Arn': instance_profile['Arn']})
-        # waits till it running
-        wait_for_ec2(bt3=config['bt3'],
-                     instance_id=instance[0].id, wait_type='start')
-        # waits till user_data == 'True' tag is set
-        custom_waiter(config, instance[0].id)
-        return instance[0].id
     except Exception as e:
-        print(repr(e))
+        print(e)
         raise e
--- lambdas/model_retrieval_lambda/get_models.py
+++ lambdas/model_retrieval_lambda/get_models.py
@@ -1,34 +1,54 @@
 import sys
-sys.path.append('/mnt/python')  # import dependencies installed in EFS (can ONLY import packages AFTER this step)
-from flask import jsonify
+import json
 import logging
+efs_mount = '/mnt/python/'
+sys.path.append(efs_mount)  # import dependencies installed in EFS (can ONLY import EFS packages AFTER this step)
+# import syft as sy
 
 logging.getLogger().setLevel(logging.INFO)
 
+'''
+Functions to add
+1. Given a particular username, return a list of all model names and versions associated with the user (DONE)
+2. Given a model name and version, allow user to download model
+    - Check out artificien experimental for methods to download the model from PyGrid
+    - Look into converting the model into an H5
+    - Look into how to download/ send back the H5.
+3. Given a model name and version, allow user to view model information/ params.
 
-# Functions to add
-# 1. Given a particular username, return a list of all model names and versions associated with the user (DONE)
-# 2. Given a model name and version, allow user to download model
-#    - Check out artificien experimental for methods to download the model from PyGrid
-#    - Look into converting the model into an H5
-#    - Look into how to download/ send back the H5.
-# 3. Given a model name and version, allow user to view model information/ params.
+Requirements for the response:
+1. 'body' property must be a 'JSON string' (i.e. use json.dumps on whatever dictionary you want to pass
+    in the body)
+2. headers is just a standard dictionary
+3. The following fields are required:
+    {
+    "isBase64Encoded": true|false,
+    "statusCode": httpStatusCode,
+    "headers": { "headerName": "headerValue", ... },
+    "body": "..."
+    }
+'''
+logging.getLogger().setLevel(logging.INFO)
 
 
 def default():
     logging.info('Successfully served GET/ method')
-    return jsonify({'statusCode': 404,
-                    'headers': {},
-                    'body': {'message': 'ERROR: Method not found'}
-                    })
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 404,
+        'headers': {},
+        'body': json.dumps({'message': 'ERROR: Method not found'})
+    }
 
 
 def test():
     logging.info('Successfully served GET /test method')
-    return jsonify({'statusCode': 200,
-                    'headers': {},
-                    'body': {'message': 'You\'ve successfully invoked the test method'}
-                    })
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps({'message': "You've successfully invoked the test method"})
+    }
 
 
 def lambda_handler(event, context):
@@ -45,20 +65,27 @@ def lambda_handler(event, context):
 
         else:
             # We only accept GET for now
-            return jsonify({'statusCode': 400,
-                            'headers': {},
-                            'body': 'We only accept GET methods right now'})
+            return {
+                'isBase64Encoded': False,
+                'statusCode': 400,
+                'headers': {},
+                'body': json.dumps('We only accept GET methods right now')
+            }
 
     except KeyError:
         logging.warning('Not a proper REST call', exc_info=True)  # prints stack trace
-        return jsonify({'statusCode': 400,
-                        'headers': {},
-                        'body': {'message': 'Not a proper REST call. No HTTP method or path found.'}
-                        })
+        return {
+            'isBase64Encoded': False,
+            'statusCode': 400,
+            'headers': {},
+            'body': json.dumps({'message': 'Not a proper REST call. No HTTP method or path found.'})
+        }
 
     except Exception:
         logging.error('Unexpected Error', exc_info=True)  # prints stack trace
-        return jsonify({'statusCode': 500,
-                        'headers': {},
-                        'body': {'message': 'Unexpected error occurred.'}
-                        })
+        return {
+            'isBase64Encoded': False,
+            'statusCode': 500,
+            'headers': {},
+            'body': json.dumps({'message': 'Unexpected error occurred.'})
+        }
