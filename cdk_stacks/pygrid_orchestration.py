3cd9f56594f97ad5d2deccd04fedde1dd7594b91
Jake Epstein <jakeepstein@Jakes-MacBook-Pro-272.local>
2021-01-25 13:49:48 -0500

    database handshaking

--- cdk_stacks/pygrid_orchestration.py
+++ cdk_stacks/pygrid_orchestration.py
@@ -3,6 +3,8 @@ from boto3.dynamodb.conditions import Key
 from flask import Flask, jsonify, request
 from pygrid_node_stack import PygridNodeStack
 from aws_cdk import core
+from artificien_infrastructure.post_deploy_actions import get_outputs
+import os
 
 app = Flask(__name__)
 region_name = "us-east-1"
@@ -12,6 +14,9 @@ try:
 
 except BaseException as exe:
     print(exe)
+
+dynamodb = boto3.resource('dynamodb', region_name=region_name)
+table = dynamodb.Table('model_table')
 
 #check api status, ping to test
 @app.route("/")
@@ -27,8 +32,6 @@ def get_endpoint(app_id):
 @app.route("/apps", methods=["POST"])
 def create_node():
     #pull up dynamo, query
-    dynamodb = boto3.resource('dynamodb', region_name=region_name)
-    table = dynamodb.Table('model_table')
     app_id = request.json.get('app_id')
     try:
         response = table.query(
@@ -42,7 +45,7 @@ def create_node():
 
     try:
         response2 = table.query(
-            KeyConditionExpression=Key('node').eq(app_id)
+            KeyConditionExpression=Key('hasNode').eq(app_id)
         )
     except:
         return jsonify({'error': 'failed to query dynamodb'}), 400
@@ -53,6 +56,50 @@ def create_node():
     app = core.App()
     PygridNodeStack(app, app_id, env=core.Environment(region="us-east-1"))
 
+    cmd = "cdk synth %s" % (app_id)
+    os.system(cmd)
+
+    cmd = "cdk deploy %s" % (app_id)
+    os.system(cmd)
+
+    #get public ip-endpoint
+    output_dict = get_outputs(stack_name=app_id)
+    if output_dict is None:
+        return
+    instance_id = output_dict['PyGridNodeLoadBalancerDNS']
+
+    #add endpoint to db, set hasNode to true
+    response = table.get_item(Key={'app_id':app_id})
+    item = response['Item']
+    item['hasNode'] = 'True'
+    item['nodeAddress'] = instance_id
+
+#delete node of an app developer
+@app.route("/apps", methods=["POST"])
+def delete_node():
+    app_id = request.json.get('app_id')
+    try:
+        response = table.query(
+            KeyConditionExpression=Key('app_id').eq(app_id)
+        )
+    except:
+        return jsonify({'error': 'failed to query dynamodb'}), 400
+
+    if response['Items'] is None:
+        return jsonify({'error': 'app id not found'}), 400
+
+    try:
+        response2 = table.query(
+            KeyConditionExpression=Key('hasNode').eq(app_id)
+        )
+    except:
+        return jsonify({'error': 'failed to query dynamodb'}), 400
+
+    if not response['response2']:
+        return jsonify({'error': 'no node exists for given app_id'}), 400
+
+    cmd = "cdk destroy %s" % (app_id)
+    os.system(cmd)
 
 
 
