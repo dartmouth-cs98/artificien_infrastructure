2dc080d21457e4ed9ea41a6ff89ef6aa381755ce
Jake Epstein <jakeepstein@Jakes-MacBook-Pro-272.local>
2021-02-02 14:00:16 -0500

    updated constants

--- pygrid_orchestration/pygrid_orchestration.py
+++ pygrid_orchestration/pygrid_orchestration.py
@@ -28,10 +28,8 @@ from websocket import create_connection
 import websockets
 import json
 import requests
-from socket_handler import SocketHandler
-
-# Singleton socket handler
-handler = SocketHandler()
+import jsonpickle.ext.numpy as jsonpickle_numpy
+jsonpickle_numpy.register_handlers()
 
 sy.make_hook(globals())
 hook.local_worker.framework = None  # force protobuf serialization for tensors
@@ -54,7 +52,7 @@ def status():
     return "Running"
 
 #spin up a new node for an app developer
-@app.route("/apps", methods=["POST"])
+@app.route("/create", methods=["POST"])
 def create_node():
     # grab model id, query model_table to check if a node has already been spun up for model
     model_table = dynamodb.Table('model_table')
@@ -83,8 +81,8 @@ def create_node():
 
     #if node hasn't been loaded yet, first validate the user has access to data
     owner = model_response['Items'][0]['owner_name']
-    if validate_user(model_id, owner) is False:
-        return jsonify({'error': 'user has not purchased requested dataset'}), 600
+   #if validate_user(model_id, owner) is False:
+   #     return jsonify({'error': 'user has not purchased requested dataset'}), 600
 
     # deploy resources
     app_factory = AppFactory()
@@ -105,77 +103,25 @@ def delete_node():
 
 @app.route("/send", methods=["POST"])
 def send_model():
-    model_table = dynamodb.Table('model_table')
-
-    training_plan_pkl = request.json.get('training_plan')
+    #model_table = dynamodb.Table('model_table')
+    #model_pkl = request.json.get('model')
+    #x_pkl = request.json.get('x')
+    #y_pkl = request.json.get('y')
+    #training_plan_pkl = request.json.get('training_plan_func')
+    #optim_func_pkl = request.json.get('optim')
+    #loss_func_pkl = request.json.get('loss')
+    #model = jsonpickle.encode(model_pkl)
+    #x = jsonpickle.decode(x_pkl)
+    #y = jsonpickle.decode(y_pkl)
     #training_plan = jsonpickle.decode(training_plan_pkl)
-    training_plan = protobuf.serde._unbufferize(hook.local_worker, training_plan_pkl)
+    #optim_func = jsonpickle.decode(optim_func_pkl)
+    #loss_func = jsonpickle.decode(loss_func_pkl)
 
-    model_params_pkl = request.json.get('model_params')
-    #model_params = jsonpickle.decode(model_params_pkl)
-    model_params = protobuf.serde._unbufferize(hook.local_worker, model_params_pkl)
-
-    avg_plan_pkl = request.json.get('avg_plan')
-    #avg_plan = jsonpickle.decode(avg_plan_pkl)
-    avg_plan = protobuf.serde._unbufferize(hook.local_worker, avg_plan_pkl)
-
-    name = request.json.get('name')
-    version = request.json.get('version')
-    batch_size = request.json.get('batch_size')
-    learning_rate = request.json.get('learning_rate')
-    max_updates = request.json.get('max_updates')
-
-    try:
-        model_response = model_table.query(KeyConditionExpression=Key('model_id').eq(name))
-    except:
-        return jsonify({'error': 'failed to query dynamodb'}), 500
-
-    if model_response['Items'] is None:
-        return jsonify({'error': 'model id not found'}), 400
-    gridAddress = model_response['Items'][0]['nodeURL']
-    gridAddress = gridAddress + ':5000'
-    print(gridAddress)
-    grid = ModelCentricFLClient(id="test", address=gridAddress, secure=False)
-    grid.connect()
-
-    client_config = {
-        "name": name,
-        "version": version,
-        "batch_size": batch_size,
-        "lr": learning_rate,
-        "max_updates": max_updates  # custom syft.js option that limits number of training loops per worker
-    }
-
-    server_config = {
-        "min_workers": 5,
-        "max_workers": 5,
-        "pool_selection": "random",
-        "do_not_reuse_workers_until_cycle": 6,
-        "cycle_length": 28800,  # max cycle length in seconds
-        "num_cycles": 5,  # max number of cycles
-        "max_diffs": 1,  # number of diffs to collect before avg
-        "minimum_upload_speed": 0,
-        "minimum_download_speed": 0,
-        "iterative_plan": True  # tells PyGrid that avg plan is executed per diff
-    }
-
-    model_params_state = State(
-        state_placeholders=[
-            PlaceHolder().instantiate(param)
-            for param in model_params
-        ]
-    )
-
-    response = grid.host_federated_training(
-        model=model_params_state,
-        client_plans={'training_plan': training_plan},
-        client_protocols={},
-        server_averaging_plan=avg_plan,
-        client_config=client_config,
-        server_config=server_config
-    )
-
-    return print("Host response:", response)
+    #model_params, training_plan = syftfunctions.def_training_plan(model, x, y, None)
+    #avg_plan = syftfunctions.def_avg_plan(model_params, None)
+    #grid = syftfunctions.artificien_connect()
+    #syftfunctions.send_model("perceptron", "0.1.1", "5", "0.5", "10", model_params, grid, training_plan, avg_plan)
+    return None
 
 
 def validate_user(model_id, owner):
@@ -186,9 +132,8 @@ def validate_user(model_id, owner):
     except:
         return jsonify({'error': 'failed to query dynamodb'}), 500
 
-    if user_response['Items'] is None:
+    if user_response['Items'][0] is None:
         return jsonify({'error': 'user not found'}), 400
-
     training_set = request.json.get('training_set')
     purchases = user_response['Items'][0]['datasets_purchased']
     if model_id in purchases:
