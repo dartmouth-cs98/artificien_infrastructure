3dfff66aed26758c06e125566569e0f5352255b6
Matt Kenney <mtnkenney2@gmail.com>
2021-02-04 16:58:03 -0500

    migrating pygrid code

--- node/local_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export NODE_ID="testnode4"
-export ADDRESS="http://localhost:5000"
-export PORT="5000"
-export MASTER_NODE_URL="http://localhost:5001"
-export DATABASE_URL="postgresql://pygridUser:abadpassword@localhost:5432/"
